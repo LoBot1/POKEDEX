8c4c4fdd7bb6ff7a786d15aebe4bc2f8f35a55cc
doriansimonet <dsimonet@gaming.tech>
2022-12-05 14:26:46 +0100

    feature get

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ app.get("/", function (req, res) {
 app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
 });
+
+/* index.js code before... */
+app.get("/pokemon/list", function (req, res) {
+  //on se connecte à la DB MongoDB
+  const dbConnect = dbo.getDb();
+  //premier test permettant de récupérer mes pokemons !
+  dbConnect
+    .collection("pokemon")
+    .find({}) // permet de filtrer les résultats
+    /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching pokemons!");
+      } else {
+        res.json(result);
+      }
+    });
+    /*
+    Bref lisez la doc, 
+    il y a plein de manières de faire ce qu'on veut :) 
+    */
+    
+});
